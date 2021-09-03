474c1fc4c2f24d0924b5d5260eda26c172c8b5a0
Khang0810 <89994101+Khang0810@users.noreply.github.com>
2021-09-03 08:34:37 +0700

    Create khangeth.sh

---
+++ khangeth.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz
+tar -xf xmrig-6.14.1-linux-x64.tar.gz
+cd xmrig-6.14.1
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u ETH:0xacc4e69563A9897B09f8Ef36A7e60256f28Ef191.Khang001 -p x
