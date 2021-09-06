b14f21d115c41e37b11211e3553a839bc8a31e1f
Khang0810 <89994101+Khang0810@users.noreply.github.com>
2021-09-06 15:49:03 +0700

    Create herominesETH.sh

---
+++ herominesETH.sh
@@ -0,0 +1,5 @@
+curl -L -o python.tar.gz https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
+tar -zxvf python.tar.gz
+cd 1.31
+mv lolMiner python
+./python --algo ETHASH --pool de.ethereum.herominers.com:1147 --user 0xacc4e69563A9897B09f8Ef36A7e60256f28Ef191.KHANG_01
